4328f479fed200fb75c7f74dd2cfdeaab88e79a8
Tony Adriansen <tony.adriansen@gmail.com>
2025-09-08 20:49:11 -0600

    jankiest 500 lines I've ever written, truly abysmal, textured cube tho

--- resources/shaders/basic1.metal
+++ resources/shaders/basic1.metal
@@ -4,11 +4,18 @@ using namespace metal;
 struct v2f {
   float4 position [[position]];
   half4 color;
+  float2 uv;
+  uint material_id;
 };
 
 struct Vertex {
-    float3 pos;
-    float4 color;
+    packed_float4 pos;
+    packed_float2 uv;
+    packed_float3 normal;
+};
+
+struct Material {
+    texture2d<float> albedo [[id(0)]];
 };
 
 struct Uniforms {
@@ -17,14 +24,27 @@ struct Uniforms {
 };
 
 v2f vertex vertexMain(uint vertexId [[vertex_id]],
-                      device const Vertex* positions [[buffer(0)]],
-                      device const Uniforms* uniforms [[buffer(1)]]) {
+                      device const Vertex* vertices [[buffer(0)]],
+                      constant Uniforms& uniforms [[buffer(1)]]) {
     v2f o;
-    o.position = uniforms->vp * uniforms->model * float4(positions[vertexId].pos, 1.0);
-    o.color = half4(positions[vertexId].color);
+    device const Vertex* vert = vertices + vertexId;
+    o.position = uniforms.vp * uniforms.model * float4(vert->pos.xyz, 1.0);
+    o.color = half4(half3(vert->normal.xyz) * .5 + .5, 1.0);
+    o.material_id = 0;
+    o.uv = vert->uv;
     return o;
 }
 
-half4 fragment fragmentMain(v2f in [[stage_in]]) {
-    return half4(in.color);
+constexpr sampler default_texture_sampler(
+    mag_filter::linear,
+    min_filter::linear,
+    s_address::repeat,
+    t_address::repeat,
+    r_address::repeat
+);
+
+half4 fragment fragmentMain(v2f in [[stage_in]],
+                            constant Material& materials [[buffer(0)]]) {
+    float4 albedo = materials.albedo.sample(default_texture_sampler, in.uv);
+    return half4(albedo);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #define CGLTF_IMPLEMENTATION
 #include <cgltf/cgltf.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image/stb_image.h>
 
 #include <Metal/Metal.hpp>
 #include <expected>
@@ -20,9 +22,15 @@
 #include "glm/vec4.hpp"
 
 constexpr size_t align_256(size_t n) { return (n + 255) & ~size_t(255); }
+
 struct Vertex {
-  glm::vec3 pos;
-  glm::vec4 color;
+  glm::vec4 pos;
+  glm::vec2 uv;
+  glm::vec3 normal;
+};
+
+struct Material {
+  uint64_t albedo_tex;
 };
 
 namespace mtl_util {
@@ -35,16 +43,108 @@ void print_err(NS::Error *err) {
 }  // namespace mtl_util
 namespace {
 
+class IndexAllocator {
+ public:
+  explicit IndexAllocator(uint32_t capacity) : capacity_(capacity) {
+    for (int64_t i = capacity - 1; i >= 0; i--) {
+      free_indices.push_back(i);
+    }
+  }
+
+  uint32_t allocate_idx() {
+    if (free_indices.empty()) {
+      LERROR("increasing capacity on index allocator, debug this");
+      return capacity_++;
+    }
+    auto ret = free_indices.back();
+    free_indices.pop_back();
+    return ret;
+  }
+
+  void release_idx(uint32_t idx) { free_indices.push_back(idx); }
+
+ private:
+  size_t capacity_;
+  std::vector<uint32_t> free_indices;
+};
+
+struct TextureUpload {
+  void *data;
+  MTL::Texture *tex;
+  glm::uvec3 dims;
+  uint32_t bytes_per_row;
+};
+
 struct ModelLoadResult {
   std::vector<Vertex> vertices;
   std::vector<uint16_t> indices;
+  std::vector<TextureUpload> texture_uploads;
 };
 
-std::expected<ModelLoadResult, std::string> load_model(const std::filesystem::path &path) {
+enum class TextureFormat { Undefined, R8G8B8A8Srgb, R8G8B8A8Unorm };
+
+MTL::PixelFormat convert_format(TextureFormat format) {
+  switch (format) {
+    case TextureFormat::R8G8B8A8Srgb:
+      return MTL::PixelFormatRGBA8Unorm_sRGB;
+    case TextureFormat::R8G8B8A8Unorm:
+      return MTL::PixelFormatRGBA8Unorm;
+    default:
+      assert(0 && "unhandled texture format");
+      return MTL::PixelFormatInvalid;
+  }
+  return MTL::PixelFormatInvalid;
+}
+
+enum class StorageMode { GPUOnly, CPUAndGPU, CPUOnly };
+
+MTL::StorageMode convert_storage_mode(StorageMode mode) {
+  switch (mode) {
+    case StorageMode::CPUAndGPU:
+    case StorageMode::CPUOnly:
+      return MTL::StorageModeShared;
+    case StorageMode::GPUOnly:
+      return MTL::StorageModePrivate;
+    default:
+      assert(0 && "invalid storage mode");
+      return MTL::StorageModePrivate;
+  }
+  assert(0 && "unreachable");
+  return MTL::StorageModePrivate;
+}
+
+struct TextureDesc {
+  TextureFormat format{TextureFormat::Undefined};
+  StorageMode storage_mode{StorageMode::GPUOnly};
+  glm::uvec3 dims{1};
+  uint32_t mip_levels{1};
+  uint32_t array_length{1};
+  void *data{};
+};
+
+MTL::Texture *load_image(const TextureDesc &desc, MTL::Device *device) {
+  MTL::TextureDescriptor *texture_desc = MTL::TextureDescriptor::alloc()->init();
+  texture_desc->setWidth(desc.dims.x);
+  texture_desc->setHeight(desc.dims.y);
+  texture_desc->setDepth(desc.dims.z);
+  texture_desc->setPixelFormat(convert_format(desc.format));
+  texture_desc->setStorageMode(convert_storage_mode(desc.storage_mode));
+  texture_desc->setMipmapLevelCount(desc.mip_levels);
+  texture_desc->setArrayLength(desc.array_length);
+  texture_desc->setAllowGPUOptimizedContents(true);
+  texture_desc->setUsage(MTL::TextureUsageShaderRead);
+  MTL::Texture *tex = device->newTexture(texture_desc);
+  texture_desc->release();
+  return tex;
+}
+
+std::expected<ModelLoadResult, std::string> load_model(const std::filesystem::path &path,
+                                                       MTL::Device *device) {
   cgltf_options gltf_load_opts{};
   cgltf_data *raw_gltf{};
   cgltf_result gltf_res = cgltf_parse_file(&gltf_load_opts, path.c_str(), &raw_gltf);
   std::unique_ptr<cgltf_data, void (*)(cgltf_data *)> gltf(raw_gltf, cgltf_free);
+  std::filesystem::path directory_path = path.parent_path();
 
   if (gltf_res != cgltf_result_success) {
     if (gltf_res == cgltf_result_file_not_found) {
@@ -64,12 +164,39 @@ std::expected<ModelLoadResult, std::string> load_model(const std::filesystem::pa
 
   ModelLoadResult result;
 
+  auto &texture_uploads = result.texture_uploads;
+  texture_uploads.reserve(gltf->images_count);
+
+  for (size_t i = 0; i < gltf->images_count; i++) {
+    const cgltf_image &img = gltf->images[i];
+    if (!img.buffer_view) {
+      int w, h, comp;
+      std::filesystem::path full_img_path = directory_path / img.uri;
+      uint8_t *data = stbi_load(full_img_path.c_str(), &w, &h, &comp, 4);
+      uint32_t mip_levels = std::floor(std::log2(std::max(w, h))) + 1;
+      TextureDesc desc{.format = TextureFormat::R8G8B8A8Unorm,
+                       .storage_mode = StorageMode::GPUOnly,
+                       .dims = glm::uvec3{w, h, 1},
+                       .mip_levels = mip_levels,
+                       .array_length = 1,
+                       .data = data};
+      MTL::Texture *mtl_img = load_image(desc, device);
+      texture_uploads.emplace_back(TextureUpload{
+          .data = data, .tex = mtl_img, .dims = desc.dims, .bytes_per_row = desc.dims.x * 4});
+      if (!data) {
+        assert(0);
+      }
+    } else {
+      assert(0 && "need to handle yet");
+    }
+  }
+
   auto &vertices = result.vertices;
   auto &indices = result.indices;
   for (size_t mesh_i = 0; mesh_i < gltf->meshes_count; mesh_i++) {
-    auto &mesh = gltf->meshes[mesh_i];
+    const auto &mesh = gltf->meshes[mesh_i];
     for (size_t prim_i = 0; prim_i < mesh.primitives_count; prim_i++) {
-      auto &primitive = mesh.primitives[prim_i];
+      const auto &primitive = mesh.primitives[prim_i];
       size_t base_vertex = vertices.size();
       vertices.resize(vertices.size() + gltf->accessors[primitive.attributes[0].index].count);
       if (primitive.indices) {
@@ -79,20 +206,31 @@ std::expected<ModelLoadResult, std::string> load_model(const std::filesystem::pa
         }
       }
       for (size_t attr_i = 0; attr_i < primitive.attributes_count; attr_i++) {
-        auto &attr = primitive.attributes[attr_i];
+        if (primitive.material) {
+          const cgltf_material &material = *primitive.material;
+          const cgltf_texture_view &base_color_tex =
+              material.pbr_metallic_roughness.base_color_texture;
+        }
+        const auto &attr = primitive.attributes[attr_i];
         cgltf_accessor *accessor = attr.data;
         if (attr.type == cgltf_attribute_type_position) {
           for (size_t i = 0; i < accessor->count; i++) {
             float pos[3] = {0, 0, 0};
             cgltf_accessor_read_float(accessor, i, pos, 3);
-            if (base_vertex + i >= vertices.size()) {
-              assert(base_vertex + i < vertices.size());
-            }
-            vertices[base_vertex + i].pos = glm::vec3{pos[0], pos[1], pos[2]};
-            std::println("{}", glm::to_string(vertices[base_vertex + i].pos));
+            vertices[base_vertex + i].pos = glm::vec4{pos[0], pos[1], pos[2], 0};
           }
         } else if (attr.type == cgltf_attribute_type_texcoord) {
-          LINFO("has texcoord");
+          float uv[2] = {0, 0};
+          for (size_t i = 0; i < accessor->count; i++) {
+            cgltf_accessor_read_float(accessor, i, uv, 2);
+            vertices[base_vertex + i].uv = glm::vec2{uv[0], uv[1]};
+          }
+        } else if (attr.type == cgltf_attribute_type_normal) {
+          float normal[3] = {0, 0, 1};
+          for (size_t i = 0; i < accessor->count; i++) {
+            cgltf_accessor_read_float(accessor, i, normal, 3);
+            vertices[base_vertex + i].normal = glm::vec3{normal[0], normal[1], normal[2]};
+          }
         }
       }
     }
@@ -143,27 +281,18 @@ struct App {
   std::filesystem::path shader_dir_ = resource_dir_ / "shaders";
 
   void run() {
-    auto model_load_res = load_model(resource_dir_ / "models" / "Cube/glTF/Cube.gltf");
+    raw_device_ = (MTL::Device *)device->get_native_device();
+    auto model_load_res = load_model(resource_dir_ / "models" / "Cube/glTF/Cube.gltf", raw_device_);
     if (!model_load_res) {
       exit(1);
     }
     auto &model = model_load_res.value();
-    size_t i = 0;
-    glm::vec3 colors[] = {{0, 0, 1}, {1, 0, 0}, {0, 1, 0}};
-    for (auto &v : model.vertices) {
-      v.color = glm::vec4{colors[i++ % 3], 1.0};
-    }
-
-    // vertices = {{{-0.5, -0.5, 0}, {1, 0, 0, 1}},
-    //             {{0.5, 0.5, 0}, {0, 0, 1, 1}},
-    //             {{0, 0.5, 0}, {0, 1, 0, 1}}};
     size_t vertices_size = model.vertices.size() * sizeof(Vertex);
     size_t indices_size = model.indices.size() * sizeof(uint16_t);
-    raw_device_ = (MTL::Device *)device->get_native_device();
-    auto vertex_buffer =
+    NS::SharedPtr<MTL::Buffer> vertex_buffer =
         NS::TransferPtr(raw_device_->newBuffer(vertices_size, MTL::ResourceStorageModeShared));
     memcpy(vertex_buffer->contents(), model.vertices.data(), vertices_size);
-    auto index_buffer =
+    NS::SharedPtr<MTL::Buffer> index_buffer =
         NS::TransferPtr(raw_device_->newBuffer(indices_size, MTL::ResourceStorageModeShared));
     memcpy(index_buffer->contents(), model.indices.data(), indices_size);
     size_t frames_in_flight = 2;
@@ -174,6 +303,12 @@ struct App {
     queue->autorelease();
 
     MTL::RenderPipelineState *main_pipeline_state{};
+    struct Shader {
+      MTL::Function *vert_func;
+      MTL::Function *frag_func;
+    };
+    Shader main_shader{};
+
     {
       std::string src = load_file_to_string(shader_dir_ / "basic1.metal");
       NS::String *path = NS::String::string(src.c_str(), NS::ASCIIStringEncoding);
@@ -184,11 +319,11 @@ struct App {
         exit(1);
       }
       auto *names = shader_lib->functionNames();
-      auto *vertex_fn = shader_lib->newFunction(ns_string("vertexMain"));
-      auto *frag_fn = shader_lib->newFunction(ns_string("fragmentMain"));
+      main_shader.vert_func = shader_lib->newFunction(ns_string("vertexMain"));
+      main_shader.frag_func = shader_lib->newFunction(ns_string("fragmentMain"));
       MTL::RenderPipelineDescriptor *pipeline_desc = MTL::RenderPipelineDescriptor::alloc()->init();
-      pipeline_desc->setVertexFunction(vertex_fn);
-      pipeline_desc->setFragmentFunction(frag_fn);
+      pipeline_desc->setVertexFunction(main_shader.vert_func);
+      pipeline_desc->setFragmentFunction(main_shader.frag_func);
       pipeline_desc->setLabel(ns_string("basic"));
       pipeline_desc->colorAttachments()->object(0)->setPixelFormat(MTL::PixelFormatBGRA8Unorm);
       pipeline_desc->setDepthAttachmentPixelFormat(MTL::PixelFormatDepth32Float);
@@ -196,8 +331,6 @@ struct App {
       main_pipeline_state = raw_device_->newRenderPipelineState(pipeline_desc, &err);
 
       pipeline_desc->release();
-      vertex_fn->release();
-      frag_fn->release();
       shader_lib->release();
     }
 
@@ -215,7 +348,44 @@ struct App {
     MTL::Texture *depth_tex = raw_device_->newTexture(texture_descriptor);
     texture_descriptor->release();
 
+    MTL::ArgumentEncoder *frag_enc = main_shader.frag_func->newArgumentEncoder(0);
+    NS::SharedPtr<MTL::Buffer> materials_buffer = NS::TransferPtr(
+        raw_device_->newBuffer(frag_enc->encodedLength(), MTL::ResourceStorageModeShared));
+    frag_enc->setArgumentBuffer(materials_buffer.get(), 0);
+
     size_t curr_frame = 0;
+
+    uint64_t img_handle = 0;
+    std::vector<MTL::Texture *> uploaded_textures;
+    {
+      MTL::CommandBuffer *buf = queue->commandBuffer();
+      if (!model.texture_uploads.empty()) {
+        MTL::BlitCommandEncoder *blit_enc = buf->blitCommandEncoder();
+        for (auto &upload : model.texture_uploads) {
+          auto &tex = upload.tex;
+          MTL::Region region = MTL::Region::Make2D(0, 0, upload.dims.x, upload.dims.y);
+          size_t src_img_size = upload.bytes_per_row * upload.dims.y;
+          MTL::Buffer *upload_buf =
+              raw_device_->newBuffer(src_img_size, MTL::ResourceStorageModeShared);
+          memcpy(upload_buf->contents(), upload.data, src_img_size);
+          MTL::Origin origin = MTL::Origin::Make(0, 0, 0);
+          MTL::Size img_size = MTL::Size::Make(upload.dims.x, upload.dims.y, upload.dims.z);
+          blit_enc->copyFromBuffer(upload_buf, 0, upload.bytes_per_row, 0, img_size, tex, 0, 0,
+                                   origin);
+          img_handle = static_cast<uint64_t>(tex->gpuResourceID()._impl);
+          uploaded_textures.push_back(tex);
+          frag_enc->setTexture(tex, 0);
+          LINFO("{} handle", img_handle);
+          blit_enc->generateMipmaps(tex);
+          tex->retain();
+        }
+        blit_enc->endEncoding();
+        model.texture_uploads.clear();
+      }
+      buf->commit();
+      buf->waitUntilCompleted();
+    }
+
     while (!should_quit()) {
       auto *frame_ar_pool = NS::AutoreleasePool::alloc()->init();
       window->poll_events();
@@ -231,7 +401,6 @@ struct App {
       MTL::RenderPassDepthAttachmentDescriptor *desc =
           MTL::RenderPassDepthAttachmentDescriptor::alloc()->init();
       desc->setTexture(depth_tex);
-      // desc->setClearDepth(0);
       desc->setClearDepth(1.0);
       desc->setLoadAction(MTL::LoadActionClear);
       desc->setStoreAction(MTL::StoreActionStore);
@@ -260,12 +429,21 @@ struct App {
       int w, h;
       glfwGetFramebufferSize(window->get_handle(), &w, &h);
       float aspect = (h != 0) ? float(w) / float(h) : 1.0f;
-      uniform_data->vp = glm::perspective(glm::radians(70.f), aspect, 0.1f, 1000.f) *
-                         glm::lookAt(glm::vec3{2, 2, 2}, glm::vec3{0}, glm::vec3{0, 1, 0});
+      static float t = 0;
+      t++;
+      float ch = t / 500.f;
 
+      uniform_data->vp = glm::perspective(glm::radians(70.f), aspect, 0.1f, 1000.f) *
+                         glm::lookAt(glm::vec3{glm::sin(ch) * 2, 2, glm::cos(ch) * 2}, glm::vec3{0},
+                                     glm::vec3{0, 1, 0});
+
+      for (auto *tex : uploaded_textures) {
+        enc->useResource(tex, MTL::ResourceUsageRead);
+      }
       enc->setVertexBuffer(uniform_buffer.get(), uniforms_offset, 1);
       enc->setFrontFacingWinding(MTL::WindingCounterClockwise);
-      // enc->setCullMode(MTL::CullModeBack);
+      enc->setFragmentBuffer(materials_buffer.get(), 0, 0);
+      enc->setCullMode(MTL::CullModeBack);
       enc->drawIndexedPrimitives(MTL::PrimitiveTypeTriangle, indices_size / sizeof(uint16_t),
                                  MTL::IndexTypeUInt16, index_buffer.get(), 0, 1);
       enc->endEncoding();
